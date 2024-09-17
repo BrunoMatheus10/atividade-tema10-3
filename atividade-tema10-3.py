d2517775012cd10236561401ebe3dcf565afb8ac
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-17 11:26:51 -0300

    Add files via upload

---
+++ atividade-tema10-3.py
@@ -0,0 +1,10 @@
+valores ={10,20,30,40,50}
+
+maior_num = max(valores)
+menor_num = min(valores)
+
+diferenca = maior_num - menor_num
+
+print(valores)
+print("o maior numero dessa lista é",maior_num, "e o menor é" ,menor_num, "e a diferença entre o maior e menor numero é",diferenca,)
+
